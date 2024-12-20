eaa64d26c3f7e7624eba0146f8786194b7b0361e
Your Name <golamsarwar1896@gmail.com>
2024-12-21 00:03:26 +0600

    implemented filter, sort and reset functionality

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -2,21 +2,31 @@
 import { useEffect, useState } from "react";
 import JobCard from "../components/JobCard";
 import axios from "axios";
+import { use } from "react";
 
 const AllJobs = () => {
   const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
   const [jobs, setJobs] = useState([]);
+
+  //all jobs, filtering, search, sort, reset
   useEffect(() => {
     const fetchAllJobs = async () => {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_API_URL}/all-jobs?filter=${filter}`
+        `${import.meta.env.VITE_API_URL}/all-jobs?filter=${filter}&sort=${sort}`
       );
       setJobs(data);
     };
     fetchAllJobs();
-  }, [filter]);
+  }, [filter, search, sort]);
 
-  console.log(filter);
+  const handleReset = () => {
+    setFilter("");
+    setSearch("");
+    setSort("");
+  };
+  console.log(search);
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
@@ -26,6 +36,7 @@ const AllJobs = () => {
               name="category"
               id="category"
               onChange={(e) => setFilter(e.target.value)}
+              value={filter}
             >
               <option value="">Filter By Category</option>
               <option value="Web Development">Web Development</option>
@@ -34,25 +45,28 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
-            <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-              <input
-                className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
-                type="text"
-                name="search"
-                placeholder="Enter Job Title"
-                aria-label="Enter Job Title"
-              />
+          <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+            <input
+              className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+              type="text"
+              name="search"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              placeholder="Enter Job Title"
+              aria-label="Enter Job Title"
+            />
 
-              <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
-                Search
-              </button>
-            </div>
-          </form>
+            <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
+              Search
+            </button>
+          </div>
+
           <div>
             <select
               name="category"
               id="category"
+              onChange={(e) => setSort(e.target.value)}
+              value={sort}
               className="border p-4 rounded-md"
             >
               <option value="">Sort By Deadline</option>
@@ -60,7 +74,9 @@ const AllJobs = () => {
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job) => (
